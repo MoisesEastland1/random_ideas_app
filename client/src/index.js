12e27d303512e494a09636b048aa95c808aff7ba
MoisesEastland1 <meastland977@gmail.com>
2026-05-18 17:57:24 -0500

    added to index.js

--- client/src/index.js
+++ client/src/index.js
@@ -4,11 +4,12 @@ import IdeaForm from './components/IdeaForm';
 import IdeaList from './components/IdeaList';
 import './css/style.css';
 
-const modal = new Modal();
+new Modal();
+
 const ideaForm = new IdeaForm();
 ideaForm.render();
 
-const ideaList = new IdeaList();
-ideaList.render();
+new IdeaList();
+
 
 
