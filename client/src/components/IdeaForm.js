7dda64d5a828a8661beb8f41483f3ef9a806b349
MoisesEastland1 <meastland977@gmail.com>
2026-05-23 17:14:11 -0500

    fixed a bug

--- client/src/components/IdeaForm.js
+++ client/src/components/IdeaForm.js
@@ -1,4 +1,4 @@
-import IdeasApi from"../services/ideasApi.js";
+import IdeasApi from"../services/ideasApi";
 import IdeaList from './IdeaList';
 class IdeaForm {
   constructor(){
@@ -13,7 +13,7 @@ class IdeaForm {
   async handleSubmit(e) {
     e.preventDefault();
 
-    if(!this._form.elements.text.value || !this._form.element.tag,value || !this._form.elements.username.value) {
+    if(!this._form.elements.text.value || !this._form.elements.tag.value || !this._form.elements.username.value) {
       alert('Please enter all fields');
       return;
     }
@@ -26,9 +26,11 @@ class IdeaForm {
       tag: this._form.elements.tag.value,
       username: this._form.elements.username.value,
     };
+
+    
     
     //Add Idea to server
-    const newIdea = await ideasApi.createIdea(idea);
+    const newIdea = await IdeasApi.createIdea(idea);
 
     //Add idea to list
     this._ideaList.addIdeaToList(newIdea.data.data);
@@ -46,23 +48,25 @@ class IdeaForm {
   render() {
     this._formModal.innerHTML = `
     <form id="idea-form">
-          <div class="form-control">
-            <label for="idea-text">Enter a Username</label>
-            <input type="text" name="username" id="username" value="${
-            localStorage.getItem('username') ? loacalStorage.getItem('username') : ''
-            }"/>
-          </div>
-          <div class="form-control">
-            <label for="idea-text">What's Your Idea?</label>
-            <textarea name="text" id="idea-text"></textarea>
-          </div>
-          <div class="form-control">
-            <label for="tag">Tag</label>
-            <input type="text" name="tag" id="tag" />
-          </div>
-          <button class="btn" type="submit" id="submit">Submit</button>
-        </form>
-    `;
+  <div class="form-control">
+<label for="idea-text">Enter a Username</label>
+  <input type="text" name="username" id="username" value="${
+  localStorage.getItem('username') ? localStorage.getItem('username') : ''}"/>
+  </div>
+
+  <div class="form-control">
+  <label for="idea-text">What's Your Idea?</label>
+  <textarea name="text" id="idea-text"></textarea>
+  </div>
+
+  <div class="form-control">
+  <label for="tag">Tag</label>
+  <input type="text" name="tag" id="tag" />
+  </div>
+  
+  <button class="btn" type="submit" id="submit">Submit</button>
+  </form>`;
+
     this._form = document.querySelector('#idea-form');
     this.addEventListeners();
   }
--- client/src/services/ideasApi.js
+++ client/src/services/ideasApi.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 class IdeasApi {
   constructor() {
-    this._apiUrl = 'http://localhost:5000/api'
+    this._apiUrl = 'http://localhost:5000/api/ideas'
   }
   getIdeas() {
     return axios.get(this._apiUrl);
@@ -10,6 +10,19 @@ class IdeasApi {
 
   createIdea(data) {
     return axios.post(this._apiUrl, data);
+  }
+
+  updateIdea(id, data) {
+    return axios.put(`${this._apiUrl}/${id}, data`);
+  }
+
+  deleteIdea(id){
+  const username = localStorage.getItem('username') ? localStorage.getItem('username') : '';
+    return axios.delete(`${this._apiUrl}/${id}`,{
+      data: {
+        username,
+      },
+    });
   }
 }
 
